41354c9aba36b122b97ff4b7b3e094ae3c3cd767
fogueteg <foguetegedalprinha@gmail.com>
2026-04-22 18:57:30 +0000

    aula

---
+++ script.js
@@ -0,0 +1,61 @@
+const turma = [
+
+    { nome : "ronaldo",
+      idade : 12,
+      nota : [10,4,6]
+    },
+    { nome : "bernardo",
+      idade : 10,
+      nota : [10,7,8]
+    },
+    { nome : "arthur",
+      idade : 11,
+      nota : [10,7,5]
+    }
+]
+function listarNomes(turma) {
+  let i = 0;
+  while (i < turma.length) {
+    console.log(turma[i].nome);
+    i++;
+  }
+}
+function calcularMedia(notas) {
+    if (!notas,notas.length === 0) {
+        return 0;
+    }
+
+    const soma = notas.reduce((total, nota) => total + nota, 0);
+    return soma / notas.length;
+}
+turma.forEach(aluno => {
+    const media = calcularMedia(aluno.nota);
+    if (media < 7)
+    console.log("Reprovado X")
+    if (media >= 7)
+    console.log("Aprovado ✔")
+    console.log("Aluno: " + aluno.nome + " " + "Media: " + media);
+});
+function adicionarAluno(nome, idade, notas) {
+    const novoAluno = {
+        nome: nome,
+        idade: idade,
+        nota: notas,
+    };
+
+    turma.push(novoAluno);
+}
+function buscarAluno(nome) {
+    let encontrado = false;
+
+    for (let i = 0; i < turma.length; i++) {
+        if (turma[i].nome.toLowerCase() === nome.toLowerCase()) {
+            encontrado = true;
+            break; }
+    {
+    if (encontrado) {
+        console.log("Aluno encontrado na turma ✔");
+    } else {
+        console.log("Aluno não está na turma ❌");
+    }
+}
